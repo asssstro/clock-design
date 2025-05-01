645e251db203620a78898e8467ea21a1de178f02
Yuhang Mao <138899874+asssstro@users.noreply.github.com>
2025-05-01 18:21:15 +0800

    Update stop_watch_tb.v

--- stop_watch_tb.v
+++ stop_watch_tb.v
@@ -1,4 +1,4 @@
-/*module stop_watch_tb;
+module stop_watch_tb;
     reg clk,rstn,isCTmode, isAlarm, isStopWatch, pause;
     reg [5:0] ct_button_in;
     wire[6:0] led_0, led_1;
@@ -17,33 +17,4 @@ top topp(clk ,rstn, led_0, led_1, led_dn, isCTmode, isAlarm, isStopWatch, ct_but
         #5 rstn = 1;
     end
     always #5 clk=~clk;
-endmodule*/
-
-module timecounter_tb;
-    reg clk,rstn,isCTmode, isAlarm, isStopWatch, pause;
-    reg [5:0] ct_button_in;
-    wire[6:0] led_0, led_1;
-    wire [5:0] led_dn;
-    top topp(clk ,rstn, led_0, led_1, led_dn, isCTmode, isAlarm, isStopWatch, ct_button_in, pause);
-    
-    initial begin
-        isStopWatch = 0;
-        isCTmode = 1;
-        isAlarm = 0;
-        rstn =0;
-        clk =1;
-        pause =1;
-        ct_button_in =6'b000000;
-        
-        #5 rstn =1;
-        
-        #100 ct_button_in =6'b100000; #20 ct_button_in =6'b000000;
-        #100 ct_button_in =6'b010000; #20 ct_button_in =6'b000000;
-        #100 ct_button_in =6'b001000; #20 ct_button_in =6'b000000;
-        #100 ct_button_in =6'b000100; #20 ct_button_in =6'b000000;
-        #100 ct_button_in =6'b000010; #20 ct_button_in =6'b000000;
-        #100 ct_button_in =6'b000001; #20 ct_button_in =6'b000000;
-    end
-    always #5 clk=~clk;
-    
 endmodule
